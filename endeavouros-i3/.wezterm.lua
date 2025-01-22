a2b9c22b889b1e2ab986d052c2f6c7b15ef2a306
Yu-Chien Peter Lin <peter.lin@sifive.com>
2025-01-22 20:46:49 +0800

    edv-i3: add wezterm config

---
+++ endeavouros-i3/.wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+local config = {}
+
+-- config.color_scheme = "Aci (Gogh)"
+config.color_scheme = "Adventure"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.7
+
+return config
